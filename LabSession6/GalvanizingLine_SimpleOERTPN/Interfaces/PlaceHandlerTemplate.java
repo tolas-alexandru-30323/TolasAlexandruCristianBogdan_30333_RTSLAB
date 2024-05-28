7651ce82e9147ecd35a680526a29c3d63b5e67c5
Tolas Cristian <bogditolas12@gmail.com>
2024-05-28 10:40:28 +0300

    Lab Homeworks

--- LabSession6/GalvanizingLine_SimpleOERTPN/Interfaces/PlaceHandlerTemplate.java
+++
@@ -1,9 +0,0 @@
-package LabSession6.GalvanizingLine_SimpleOERTPN.Interfaces;
-
-public interface PlaceHandlerTemplate {
-	public void AddPlace(PlaceTemplate place);
-
-	public PlaceTemplate GetPlaceByName(String Name);
-
-	public String PrintAllPlaces();
-}
